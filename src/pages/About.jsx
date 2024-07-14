fe4b45c541c1d0eccbee5ba34b022224814e9af6
Ganesh Vhanmane <mr.vga1415@gmail.com>
2024-07-14 16:36:54 +0530

    small change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 const About = () => {
-  return (
+  return (     
     <>
       <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
         <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
